1a8f0c9618097b4f8037e2560d2b309547e415a4
Ruby Wang <zirwang@mymail.mines.edu>
2017-10-09 13:41:06 -0600

    added click event

--- duckhunt.js
+++ duckhunt.js
@@ -1,12 +1,10 @@
-var c = document.getElementById('Game')
-var ctx = c.getContext('2d')
-var target = new Image()
 
-// http://i.imgur.com/ruZv0dl.png sees a CLEAR, CRISP image
-target.src = "duck.png"
-target.width = 32
-target.height = 32
-
-target.onload = function() {
-  ctx.drawImage(target, 0, 0, 32, 32);
-};
+$(document).ready(function(e){
+  var audio = document.createElement('audio');
+      audio.setAttribute('src', 'Shotsoundeffect.mp3');
+    $("#Game").click(function(e){
+        var mousex = e.pagex;
+        var mousey = e.pagey;
+        audio.play();
+    });
+});
